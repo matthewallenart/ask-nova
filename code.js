b2f468396b4c6a2cd12a86a704650008b1a569f1
Matthew Allen <mattallen09@live.com>
2025-08-03 02:41:31 +0100

    x

--- code.js
+++ code.js
@@ -8,4 +8,11 @@ figma.ui.onmessage = msg => {
   if (msg.type === 'resize') {
     figma.ui.resize(msg.width, msg.height);
   }
+  if (msg.type === 'reload') {
+    figma.showUI(__html__, { 
+      width: 400, 
+      height: 600,
+      themeColors: true 
+    });
+  }
 };
